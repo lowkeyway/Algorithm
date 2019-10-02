27316505e2b8230d3e562950eb464a80dd0a38e7
lowkeyway <lowkeyway@126.com>
2019-10-02 23:34:55 +0800

    FibonacciSearch

---
+++ 23_FibonacciSearch/MySearch.h
@@ -0,0 +1,35 @@
+#ifndef __MY_SEARCH_H__
+#define __MY_SEARCH_H__
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef struct searchData {
+	int data;
+	int index;
+}SearchNode;
+
+class MySearch
+{
+public:
+	MySearch(const int *pList, int iCap);
+	~MySearch();
+
+	bool listPrint(int* pList, int iLen);
+	bool listPrint(SearchNode* pListNode, int iLen);
+
+	bool sequenceSearch(int target, int &index);
+	bool binarySearch(int target, int &index);
+	bool insertSearch(int target, int &index);
+	bool fibonacciSearch(int target, int &index);
+
+public:
+	int *m_piSearchList;
+	int m_iSearchCap;
+};
+
+
+
+#endif // !__MY_SEARCH_H__
